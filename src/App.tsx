e490ca78bb8487851b26ae32ffc3ee230bb436c6
Luu Hoang Khang <khang.luu@tekmedi.com>
2020-07-20 09:22:58 +0700

    update Pagging

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,80 @@
 import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Pagination from 'react-bootstrap/Pagination'
 import PageItem from 'react-bootstrap/PageItem'
-import Select from 'react-select'
 import Table from 'react-bootstrap/Table'
-import isEmpty  from 'lodash'
-
+import Button from 'react-bootstrap/Button'
+import isEmpty from 'lodash'
 const DATA = [
   {
-  "name":"1",
+  "name":"khangKhang",
   "des" :"Luu Hoang Khang",
   "code":"12312312",
   },
   {
-    "name":"khang",
+    "name":"khangLUU",
     "des" :"Luu Hoang Khang",
     "code":"12312312",
   },
   {
-    "name":"khang",
+    "name":"khang123213",
     "des" :"Luu Hoang Khang",
     "code":"12312312",
   },
   {
-    "name":"khang",
+    "name":"khang123sad",
     "des" :"Luu Hoang Khang",
     "code":"12312312",
   },
   {
-    "name":"khang",
+    "name":"khangkhangLuu",
     "des" :"Luu Hoang Khang",
     "code":"12312312",
   },
   {
-    "name":"khang",
+    "name":"khangHHJSJ",
     "des" :"Luu Hoang Khang",
     "code":"12312312",
   },
   {
-    "name":"khang",
+    "name":"khang)()(",
     "des" :"Luu Hoang Khang",
     "code":"12312312",
     },
     {
-      "name":"khang",
+      "name":"khangLLKSKS",
       "des" :"Luu Hoang Khang",
       "code":"12312312",
     },
     {
-      "name":"khang",
+      "name":"khangnnn",
       "des" :"Luu Hoang Khang",
       "code":"12312312",
     },
   
 ]
 const LISTGETBY = [10,20,30,40]
-function App({data, listgetBy}) {
+function App() {
   const [listData, setListData ] = useState(DATA)
   const [currentPage, setCurrentPage] = useState(0)
   const [listDataCurrentPage, setListDataCurrentPage]= useState([])
-  const [listPage , setListPage] =  useState([[]])
-  const [getBy, setGetPage] = useState(3)
+  const [getBy, setGetPage] = useState(5)
   useEffect(() => {
-    // pagging()
-    setUpFirstPage(currentPage, getBy)
+    setUpFirstPage (getBy)
     return () => {
     }
   }, [])
   useEffect(() => {
      console.log("userEffect2 chay")
-     pushCurrentPage(currentPage, getBy)
+     pushCurrentPage(getBy)
     return () => {     
     }
   }, [currentPage])
   const RenderItem =()=> {
-    let ActiveItem = currentPage 
+    let ActiveItem : number = currentPage 
     let items = [];
-    let degree = (listData.length/getBy).toFixed(0)
-    for (let index = 0 ; index < degree ; index++)
+    let degree : number  =  parseInt((listData.length/getBy).toFixed(0))
+    for (let index : number = 0 ; index < degree ; index++)
     {
       items.push(
         <PageItem key = {index} active = {index === ActiveItem} onClick ={()=>{activePage(index)}}>
@@ -92,32 +88,14 @@ function App({data, listgetBy}) {
       </Pagination>
     )
   }
-  const activePage = index =>{
+  const activePage = (index:any) =>{
     setCurrentPage(index)
-    console.log("setPage")
   }
-  // const pagging = () =>{
-  //   console.log("page")
-  //   let pageMax = listData.length/getBy
-  //   let listItem = [[]]
-  //   for (let index = 0 ; index < pageMax; index++) 
-  //   {
-  //            listItem[index] =[]
-  //       for (let indexJ = 0 ; indexJ < getBy ; indexJ++ ) {
-            
-  //           listItem[index].push(listData[indexJ])
-  //       }
-  //       console.log(listItem[index])
-  //   }
-  //   setListPage(listItem)
-  //   console.log("affter paging  " + listItem)
-
-  // }
-    const pushCurrentPage = (currentPage, getBy) => {
-      let listItem = []      
+   const pushCurrentPage = (getBy : any) => {
+      let listItem : any = []     
       let startCut = getBy*currentPage
-      let endCut =  getBy*currentPage + getBy
-            for (let index = startCut ; index < endCut ; index++ )
+      let endCut =  (getBy*currentPage + getBy < listData.length ) ? getBy*currentPage + getBy : listData.length
+      for (let index = startCut ; index < endCut ; index++ )
               {
                 listItem.push(listData[index])
                 console.log(listData[index])
@@ -125,8 +103,8 @@ function App({data, listgetBy}) {
           setListDataCurrentPage(listItem)
           console.log(listItem)
     } 
-    const setUpFirstPage = getBy => {
-      let listItem = []
+    const setUpFirstPage = (getBy : any) => {
+      let listItem : any = []
       for(let index = 0; index < getBy ; index++ )
       { 
         console.log("mang chay")
@@ -134,40 +112,43 @@ function App({data, listgetBy}) {
       }
        setListDataCurrentPage(listItem)
     }
+    const renderItemTable = (listData : any []) => {
+
+          return (
+            listData.map((item : any, index : number) => (
+              <tr key = {index}>
+                <td>{index+1}</td>  
+                <td>{item.name}</td>  
+                <td>{item.des}</td>  
+                <td>
+                  <tr className ="styleButtom">
+                  <Button variant="primary">Primary</Button>
+                  <Button variant="secondary">Secondary</Button>
+                  <Button variant="outline-danger">Secondary</Button>
+                  </tr>
+                </td>  
+              </tr>
+            ))
+          )
+    }
   return (
  <div className="App">
-      <Table striped bordered hover>
+  <Table striped bordered hover responsive={false}>
   <thead>
     <tr>
       <th>#</th>
       <th>Ten </th>
-      <th>des</th>
+      <th>Mota</th>
       <th>code</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-    <td>2</td>
-      <td>{(!isEmpty(listData[0])?listData[0].name.trim:"")}</td>
-      <td>{currentPage}</td>
-      <td>{currentPage}</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td colSpan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    {renderItemTable((isEmpty(listDataCurrentPage))?listDataCurrentPage:[])}
   </tbody>
-</Table>
-    <Pagination size = "lg">
-    <PageItem onClick={()=>setCurrentPage(currentPage-1)}>Trang trước</PageItem>
-    <Pagination>
+ </Table>
+    <Pagination size ="lg">
+    <PageItem onClick={()=>setCurrentPage((currentPage-1 < 0) ? currentPage : currentPage-1)}>Trang trước</PageItem>
+    <Pagination> 
       <RenderItem/>
     </Pagination>
     <PageItem onClick={()=>setCurrentPage(currentPage+1)}>Trang sau </PageItem>
